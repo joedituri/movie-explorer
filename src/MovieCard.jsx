049c891960d16c48ef5cb29fb97b9e2eb3e7b83c
joedituri <wjoedituri@gmail.com>
2026-01-04 21:55:47 -0600

    initialize project

---
+++ src/MovieCard.jsx
@@ -0,0 +1,16 @@
+function MovieCard(movie) {
+    function onFavoriteClick() {}
+
+
+
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className="favorite-btn" onClick={onFavoriteClick}></button>
+
+            </div>
+        </div>
+            
+    </div>
+}
