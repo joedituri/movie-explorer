0f794372b4033e6446f1a226156b4a1a57410096
joedituri <wjoedituri@gmail.com>
2026-01-05 22:30:39 -0600

    add components, css, api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './css/App.css'
 import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router'
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 function App() {
   const movieNumber = 11;
 
 
   return (
-    <>
-    <Home/>
-    </>
-  )
+    <div>
+      <NavBar/>
+      <main className="main-content">
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/favorites' element={<Favorites/>}></Route>
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,55 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from "react"
+import "../css/Home.css"
+import { useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
 
 function Home() {
-    const movies = [
-        {id:1, title: "John Wick", release_date: "2020"},
-        {id:2, title:"Terminator", release_date: "1999"},
-        {id:3, title: "Pokemon", release_date: "2006"}
-    ]
+    const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error,setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadPopularMovies = async() => {
+            try {
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies);
+            }
+            catch (err) {
+                console.log(err)
+                setError("Failed to Load Movies..");
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        loadPopularMovies()
+    }, [])
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("0000000")
+    }
 
     return <div className ="home">
-        <div className="movies-grid">
-            {movies.map((
-                movie => <MovieCard movie ={movie} key={movie.id}/>
-                ))}
-        </div>
+        <form onSubmit={handleSearch} className="search-form">
+            <input 
+            type="text" 
+            placeholder="Search for movies" 
+            className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Submit</button>
+        </form>
+
+        <div className="movies-grid"> 
+            {movies.map((movie => (
+                <MovieCard movie ={movie} key={movie.id}/>
+                )))}  
+        </div> 
     </div>
     }
 
