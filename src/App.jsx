e7de3fd03cc8645aaf23ab4ef03982b05877d331
joedituri <wjoedituri@gmail.com>
2026-01-06 22:25:33 -0600

    finalized project

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import Home from './pages/Home'
 import { Routes, Route } from 'react-router'
 import Favorites from './pages/Favorites'
 import NavBar from './components/NavBar'
+import { MovieProvider } from './contexts/MovieContext'
 
 function App() {
   const movieNumber = 11;
 
 
   return (
+    <MovieProvider>
     <div>
       <NavBar/>
       <main className="main-content">
@@ -22,7 +24,8 @@ function App() {
         </Routes>
       </main>
     </div>
-  );
+    </MovieProvider>
+  )
 }
 
 export default App
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,23 @@
 import "../css/Favorites.css"
+import { useMovieContext } from "../contexts/MovieContext"
+import MovieCard from "../components/MovieCard"
 
 function Favorites () {
+    const {favorites} = useMovieContext()
 
-return <div className="favorites-empty">
-    <h2>No Favorite Movies Yet</h2>
-</div>
+    if (favorites) {
+        return (
+        <div className="movies-grid"> 
+            {favorites.map((movie => (
+                <MovieCard movie ={movie} key={movie.id}/>
+                )))}
+        </div> 
+        )
+    }
+
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+    </div>
 }
 
 export default Favorites
